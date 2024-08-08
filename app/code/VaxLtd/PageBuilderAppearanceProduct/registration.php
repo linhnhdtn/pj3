875b2405bf703418e0a47c9b476888ae6f3c1f40
lbeo <linhnh@dtn.com.vn>
2024-08-08 14:08:35 +0700

    dsd

---
+++ app/code/VaxLtd/PageBuilderAppearanceProduct/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'VaxLtd_PageBuilderAppearanceProduct',
+    __DIR__
+);
